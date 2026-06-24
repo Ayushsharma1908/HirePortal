abd839757d9c38cea09d0c575ee16ae7932fb05b
Ayush Kumar Sharma <ayushsharma192004@gmail.com>
2026-06-24 15:58:33 +0530

    Updated baseURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,23 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('adminToken')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+  const token = localStorage.getItem("adminToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem('adminToken')
-      localStorage.removeItem('adminUser')
-      window.location.href = '/admin/login'
+      localStorage.removeItem("adminToken");
+      localStorage.removeItem("adminUser");
+      window.location.href = "/admin/login";
     }
-    return Promise.reject(err)
-  }
-)
 
-export default api
+    return Promise.reject(err);
+  }
+);
+
+export default api;
